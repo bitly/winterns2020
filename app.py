e5193746433c7d9f2c47e3aaf1f56bf4a1730cd2
gracemcgrath <46832828+gracemcgrath@users.noreply.github.com>
2020-01-03 14:54:00 -0500

    Adding stuff (#1)
    * adding basic html and .gitignore

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask import render_template 
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+    hi = "Hello Winterns!"
+    return render_template('app.html', text=hi)

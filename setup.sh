c493ac2ac52d9a44661a2c0ad669311b4133f22e
Gabriel Strauss <gstrauss5@gmail.com>
2020-01-06 21:59:37 +0000

    Removes environmental variable setter in setup.sh.

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 cd /workspace/winterns2020
 
 # Changes to your flask app will be reflected without having to restart the app.
-export FLASK_ENVIRONMENT=development
-
 # install js requirements
 npm install
 

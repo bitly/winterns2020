b44da7c482f487f0355069bc13a218180e857f53
hwilliams27 <hannahdwilliams27@gmail.com>
2020-01-09 22:16:01 +0000

    I made a requests.post that allows you to create a bitly link!!

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import requests
 import json
 app = Flask(__name__)
 
-@app.route("/api", methods=['POST'])
+@app.route("/api", methods=['GET','POST'])
 def hello():
     hi = "Hello Winterns Test"
-    response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers={"Authorization": "Bearer token"}, json={"long_url":"https://www.youtube.com/watch?v=uvgBKO7CGzY"})
+    response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers={"Authorization": "Bearer <enter token>"}, json={"long_url":"https://www.youtube.com/watch?v=uvgBKO7CGzY"})
 
     print(json.loads(response.text))
     return render_template('app.html', text=hi)

78bcf05761e2766ee16190b64e7e7747d4b91061
hwilliams27 <hannahdwilliams27@gmail.com>
2020-01-09 21:46:01 +0000

    post request made part 2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/api", methods=['POST'])
 def hello():
     hi = "Hello Winterns Test"
-    response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers={"Authorization": "Bearer f49a5d4d7d7eacc777aa3fa67d58fec9250f9759"}, json={"long_url":"https://www.youtube.com/watch?v=uvgBKO7CGzY"})
+    response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers={"Authorization": "Bearer token"}, json={"long_url":"https://www.youtube.com/watch?v=uvgBKO7CGzY"})
 
     print(json.loads(response.text))
     return render_template('app.html', text=hi)

75bc8da123389c4a5de97ef98750cd78813b3e68
Aminata Jaiteh <aminatajaiteh24@gmail.com>
2020-01-10 20:54:13 +0000

    made the buttons work and added an handeler to make it print this worked in the console

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import render_template 
+import requests
+import json 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET'])
 def hello():
     hi = "Hello Winterns Test. Katya. "
     return render_template('app.html', text=hi)

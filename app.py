d288407b10746f55db7ca642daae5f9d646a9560
hwilliams27 <hannahdwilliams27@gmail.com>
2020-01-10 20:22:28 +0000

    wrote get user and post expanded link functions that can connect to FE

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template 
+import requests, json
 
 app = Flask(__name__)
 
@@ -7,3 +8,18 @@ app = Flask(__name__)
 def hello():
     hi = "Hello Winterns Test. Katya. "
     return render_template('app.html', text=hi)
+
+
+@app.route('/api', methods=['GET', 'POST'])
+def quote():
+    response = requests.get('https://api-ssl.bitly.com/v4/user', headers={"Authorization": "Bearer token"})
+    
+    return (response.json())
+
+@app.route('/api', methods=['GET', 'POST'])
+def quote():
+    response = requests.post('https://api-ssl.bitly.com/v4/expand', headers={"Authorization": "Bearer token"}, json={"bitlink_id":"bit.ly/2Nbig94"})
+    
+    return (response.json())
+
+

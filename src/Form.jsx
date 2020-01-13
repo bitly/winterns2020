1d339b636081cf0c3692cf745752b05a4969e7ef
hwilliams27 <hannahdwilliams27@gmail.com>
2020-01-13 20:51:56 +0000

    made a form file for inputs and placed submit button inside

---
+++ src/Form.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+export default class Form extends React.Component {
+state = {
+    authToken: ' '
+};
+
+render(){
+    return (
+       <div>
+           <form>
+            Authentification Token: <input id="textbox" type="text" value={this.state.authToken} />
+            Authentification Token: <input id="textbox" type="text" />
+             <button onClick={this.handleClick}>SUBMIT!</button> 
+           </form>
+       </div>
+    )
+}   
+}

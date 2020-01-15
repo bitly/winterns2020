3174a93c58ae8cdbed5a43507178d66988d40db4
hwilliams27 <hannahdwilliams27@gmail.com>
2020-01-15 22:11:23 +0000

    added endpoint variable to request.get

--- app.py
+++ app.py
@@ -17,9 +17,10 @@ def hello():
 def quote():
     
     auth_token = request.json['authToken'] 
+    endpoint = request.json['apiEndpoint']
 
-    response = requests.get('https://api-ssl.bitly.com/v4/user', headers={"Authorization": "Bearer " + auth_token})
-     
+    response = requests.get('https://api-ssl.bitly.com/v4' + endpoint, headers={"Authorization": "Bearer " + auth_token})
+ 
     return (response.json()) 
 
 

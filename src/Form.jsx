3275ede659511cf0612285d74fefdae331fccb67
Katya Civil <TheCivilAge@gmail.com>
2020-01-22 15:40:11 +0000

    Side navigation bar has function

--- src/Form.jsx
+++ src/Form.jsx
@@ -37,7 +37,7 @@ addKeyVal = (event) => {
 
 handleSubmit() {
     console.log('BUTTON CLICKED')
-    const SERVER_URL = "https://5000-db30d818-43f2-4f4b-9b62-1bcef4387d56.ws-us02.gitpod.io/"
+    const SERVER_URL = "https://5000-f9e7f7e4-c57e-43fb-8340-f9648942ba09.ws-us02.gitpod.io/"
     console.log(SERVER_URL + '/api')
         // const paramsList = this.state.params.map(item => (item.key + ":" + item.value))
     const arrayToObject = (array) =>
